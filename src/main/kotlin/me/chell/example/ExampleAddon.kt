86ee4928c8f4e6e4d85d123c8400cad69403cfaf
chell <no>
2022-01-05 16:15:28 +0100

    add name

--- src/main/kotlin/me/chell/example/ExampleAddon.kt
+++ src/main/kotlin/me/chell/example/ExampleAddon.kt
@@ -3,7 +3,7 @@ package me.chell.example
 import me.chell.samsara.api.addon.Addon
 import me.chell.samsara.api.util.Globals
 
-class ExampleAddon: Addon(), Globals {
+class ExampleAddon: Addon("Example"), Globals {
     override fun load() {
         LOG.info("Example Addon loaded.")
     }
