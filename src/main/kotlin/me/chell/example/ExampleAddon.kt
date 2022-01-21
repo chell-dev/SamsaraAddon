588b0b86bac2413adb9e6d1aed381aec7b66679c
chell <no>
2022-01-21 17:11:58 +0100

    module example

--- src/main/kotlin/me/chell/example/ExampleAddon.kt
+++ src/main/kotlin/me/chell/example/ExampleAddon.kt
@@ -1,13 +1,21 @@
 package me.chell.example
 
 import me.chell.samsara.api.addon.Addon
+import me.chell.samsara.api.module.ModuleManager
 import me.chell.samsara.api.util.Globals
 
 class ExampleAddon: Addon("Example"), Globals {
+
+    private val module: ExampleModule = ExampleModule()
+
     override fun load() {
         LOG.info("Example Addon loaded.")
+
+        ModuleManager.modules.add(module)
+        module.load()
     }
 
     override fun unload() {
+        module.unload()
     }
 }
---
+++ src/main/kotlin/me/chell/example/ExampleModule.kt
@@ -0,0 +1,24 @@
+package me.chell.example
+
+import me.chell.samsara.api.event.EventHandler
+import me.chell.samsara.api.module.Module
+import me.chell.samsara.api.value.Register
+import me.chell.samsara.api.value.Value
+import me.chell.samsara.impl.event.PlayerTickEvent
+import me.chell.samsara.impl.event.RenderHudEvent
+
+class ExampleModule: Module("Example", Category.MISC, "This is an example module.") {
+
+    @Register val setting = Value("A Value", 2.0, min = 0.0, max = 5.0)
+
+    @EventHandler
+    fun onTick(event: PlayerTickEvent) {
+        if(setting.value > 1.0)
+            player.isSprinting = true
+    }
+
+    @EventHandler
+    fun onRender2D(event: RenderHudEvent.Post) {
+        fontRenderer.draw(event.matrices, "Hi", 2f, 2f, -1)
+    }
+}
